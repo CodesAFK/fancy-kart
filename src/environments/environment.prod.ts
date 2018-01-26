1de3d183e693fb14c70881b5cff909f157d3c36a
CodesAFK <codesafk@gmail.com>
2018-01-26 02:09:02 -0600

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAeTOyN-FeK4KJhxfv2meXPVh31qLb3Ke0',
+    authDomain: 'fancy-cart.firebaseapp.com',
+    databaseURL: 'https://fancy-cart.firebaseio.com',
+    projectId: 'fancy-cart',
+    storageBucket: 'fancy-cart.appspot.com',
+    messagingSenderId: '42466470115'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAeTOyN-FeK4KJhxfv2meXPVh31qLb3Ke0',
+    authDomain: 'fancy-cart.firebaseapp.com',
+    databaseURL: 'https://fancy-cart.firebaseio.com',
+    projectId: 'fancy-cart',
+    storageBucket: 'fancy-cart.appspot.com',
+    messagingSenderId: '42466470115'
+  }
 };
